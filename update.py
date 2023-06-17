306be484e533f8153753e12fd2e6b4e80932f9d7
Nevojt <119137577+Nevojt@users.noreply.github.com>
2023-06-17 19:55:00 +0200

    Create function to show cost product in menu Update

--- update.py
+++ update.py
@@ -20,6 +20,16 @@ class Ui_MainWindow(object):
         font.setWeight(50)
         self.label.setFont(font)
         self.label.setObjectName("label")
+        
+        self.label_6 = QtWidgets.QLabel(self.centralwidget)
+        self.label_6.setGeometry(QtCore.QRect(250, 30, 150, 20))
+        font = QtGui.QFont()
+        font.setPointSize(9)
+        font.setBold(False)
+        font.setWeight(50)
+        self.label_6.setFont(font)
+        self.label_6.setObjectName("label_6")
+        
         self.comboBox = QtWidgets.QComboBox(self.centralwidget)
         self.comboBox.setGeometry(QtCore.QRect(50, 60, 140, 30))
         font = QtGui.QFont()
@@ -34,30 +44,58 @@ class Ui_MainWindow(object):
         font.setWeight(50)
         self.label_2.setFont(font)
         self.label_2.setObjectName("label_2")
+        
+        self.label_5 = QtWidgets.QLabel(self.centralwidget)
+        self.label_5.setGeometry(QtCore.QRect(250, 110, 50, 16))
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        font.setBold(True)
+        font.setWeight(50)
+        self.label_5.setFont(font)
+        self.label_5.setObjectName("label_5")
+        
         self.comboBox_2 = QtWidgets.QComboBox(self.centralwidget)
         self.comboBox_2.setGeometry(QtCore.QRect(50, 140, 140, 30))
         font = QtGui.QFont()
         font.setPointSize(9)
         self.comboBox_2.setFont(font)
         self.comboBox_2.setObjectName("comboBox_2")
+        
         self.label_3 = QtWidgets.QLabel(self.centralwidget)
         self.label_3.setGeometry(QtCore.QRect(50, 190, 131, 16))
         font = QtGui.QFont()
         font.setPointSize(9)
         self.label_3.setFont(font)
         self.label_3.setObjectName("label_3")
+        
+        self.label_7 = QtWidgets.QLabel(self.centralwidget)
+        self.label_7.setGeometry(QtCore.QRect(250, 190, 131, 16))
+        font = QtGui.QFont()
+        font.setPointSize(9)
+        self.label_7.setFont(font)
+        self.label_7.setObjectName("label_7")
+        
         self.comboBox_3 = QtWidgets.QComboBox(self.centralwidget)
         self.comboBox_3.setGeometry(QtCore.QRect(50, 220, 140, 30))
         font = QtGui.QFont()
         font.setPointSize(9)
         self.comboBox_3.setFont(font)
         self.comboBox_3.setObjectName("comboBox_3")
+        
         self.label_4 = QtWidgets.QLabel(self.centralwidget)
         self.label_4.setGeometry(QtCore.QRect(50, 270, 131, 16))
         font = QtGui.QFont()
         font.setPointSize(9)
         self.label_4.setFont(font)
         self.label_4.setObjectName("label_4")
+        
+        self.label_8 = QtWidgets.QLabel(self.centralwidget)
+        self.label_8.setGeometry(QtCore.QRect(250, 270, 131, 16))
+        font = QtGui.QFont()
+        font.setPointSize(9)
+        self.label_8.setFont(font)
+        self.label_8.setObjectName("label_8")
+        
         self.comboBox_4 = QtWidgets.QComboBox(self.centralwidget)
         self.comboBox_4.setGeometry(QtCore.QRect(50, 300, 140, 30))
         font = QtGui.QFont()
@@ -130,7 +168,9 @@ class Ui_MainWindow(object):
         self.checkBox_3.stateChanged.connect(self.check_box_3)
         self.checkBox_4.stateChanged.connect(self.check_box_4)
         self.pushButton.clicked.connect(self.button_click)
-        
+        self.comboBox.currentIndexChanged.connect(self.view_label_6)
+        self.comboBox_3.currentIndexChanged.connect(self.view_label_7)
+        self.comboBox_4.currentIndexChanged.connect(self.view_label_8)
         
         # Подія зміни в базі даних
     def button_click(self):
@@ -147,30 +187,35 @@ class Ui_MainWindow(object):
         
     # З'вязуємо функцію-обробник події чек-боксів
     def check_box_1(self, state):
-        
-        
         if state == QtCore.Qt.Checked:
             self.fill_combobox1()
+            
         else:
             self.comboBox.clear()
+            self.label_6.clear()
             
     def check_box_2(self, state):
         if state == QtCore.Qt.Checked:
             self.update_euro()
+            self.view_label_5()
+            
         else:
             self.comboBox_2.clear()
+            self.label_5.clear()
             
     def check_box_3(self, state):
         if state == QtCore.Qt.Checked:
             self.fill_combobox3()
         else:
             self.comboBox_3.clear()
+            self.label_7.clear()
             
     def check_box_4(self, state):
         if state == QtCore.Qt.Checked:
             self.fill_combobox4()
         else:
             self.comboBox_4.clear()
+            self.label_8.clear()
         
         
     #  заповнення комбо-боксів   
@@ -188,6 +233,56 @@ class Ui_MainWindow(object):
         color_values = self.cost_color()
         self.comboBox_4.addItems(color_values)
         
+    def view_label_5(self):
+        conn = sqlite3.connect('data\preforma.db')
+        curs = conn.cursor()
+        curs.execute("SELECT cena_za_kg FROM kurs WHERE surowiec= 'EURO'")
+        result = curs.fetchall()
+        data = [row[0] for row in result]
+        curs.close()
+        conn.commit()
+        conn.close()
+        self.label_5.setText(str(data))
+        return data
+    
+    def view_label_6(self):
+        sur = str(self.comboBox.currentText())
+        conn = sqlite3.connect('data\preforma.db')
+        curs = conn.cursor()
+        curs.execute(f"SELECT cena_za_kg FROM kurs WHERE surowiec= '{sur}'")
+        result = curs.fetchall()
+        data = [row[0] for row in result]
+        curs.close()
+        conn.commit()
+        conn.close()
+        self.label_6.setText(str(data))
+        return data
+    
+    def view_label_7(self):
+        machine = str(self.comboBox_3.currentText())
+        conn = sqlite3.connect('data\preforma.db')
+        curs = conn.cursor()
+        curs.execute(f"SELECT Koszt FROM data WHERE Maszyna = '{machine}'")
+        result = curs.fetchall()
+        data = str(list(set([row[0] for row in result])))
+        curs.close()
+        conn.commit()
+        conn.close()
+        self.label_7.setText(data)
+        return data
+    
+    def view_label_8(self):
+        color = str(self.comboBox_4.currentText())
+        conn = sqlite3.connect('data/barwnik.db')
+        curs = conn.cursor()
+        curs.execute(f"SELECT Cena_za_kg FROM data WHERE Identyfikator = '{color}'")
+        result = curs.fetchall()
+        data = str(list(set([row[0] for row in result])))
+        curs.close()
+        conn.commit()
+        conn.close()
+        self.label_8.setText(data)
+        return data
         
         
     # Беремо дані з бази даних
@@ -284,7 +379,7 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "Update"))
-        self.label.setText(_translate("MainWindow", f"Cena Pet / R-Pet za kg"))
+        self.label.setText(_translate("MainWindow", "Cena Pet / R-Pet za kg"))
         self.label_2.setText(_translate("MainWindow", "Kurs EUR / PLN"))
         self.label_3.setText(_translate("MainWindow", "Koszt maszyny PLN/h"))
         self.label_4.setText(_translate("MainWindow", "Barvnik cena/kg"))
